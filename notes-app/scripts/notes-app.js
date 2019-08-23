e02544ee9833494d186cc522041b4f8d0131effd
Randy Vianda Putra <modulloe@gmail.com>
2019-08-24 03:24:13 +0700

    add js

--- notes-app/scripts/notes-app.js
+++ notes-app/scripts/notes-app.js
@@ -3,40 +3,40 @@
 let notes = getSavedNotes()
 
 const filters = {
-    searchText: '',
-    sortBy: 'byEdited'
+  searchText: '',
+  sortBy: 'byEdited',
 }
 
 renderNotes(notes, filters)
 
-document.querySelector('#create-note').addEventListener('click', (e) => {
-    const id = uuidv4()
-    const timestamp = moment().valueOf()
+document.querySelector('#create-note').addEventListener('click', e => {
+  const id = uuidv4()
+  const timestamp = moment().valueOf()
 
-    notes.push({
-        id: id,
-        title: '',
-        body: '',
-        createdAt: timestamp,
-        updatedAt: timestamp
-    })
-    saveNotes(notes)
-    location.assign(`/edit.html#${id}`)
+  notes.push({
+    id: id,
+    title: '',
+    body: '',
+    createdAt: timestamp,
+    updatedAt: timestamp,
+  })
+  saveNotes(notes)
+  location.assign(`/edit.html#${id}`)
 })
 
-document.querySelector('#search-text').addEventListener('input', (e) => {
-    filters.searchText = e.target.value
+document.querySelector('#search-text').addEventListener('input', e => {
+  filters.searchText = e.target.value
+  renderNotes(notes, filters)
+})
+
+document.querySelector('#filter-by').addEventListener('change', e => {
+  filters.sortBy = e.target.value
+  renderNotes(notes, filters)
+})
+
+window.addEventListener('storage', e => {
+  if (e.key === 'notes') {
+    notes = JSON.parse(e.newValue)
     renderNotes(notes, filters)
-})
-
-document.querySelector('#filter-by').addEventListener('change', (e) => {
-    filters.sortBy = e.target.value
-    renderNotes(notes, filters)
-})
-
-window.addEventListener('storage', (e) => {
-    if (e.key === 'notes') {
-        notes = JSON.parse(e.newValue)
-        renderNotes(notes, filters)
-    }
+  }
 })
--- notes-app/scripts/notes-edit.js
+++ notes-app/scripts/notes-edit.js
@@ -6,47 +6,47 @@ const removeElement = document.querySelector('#remove-note')
 const dateElement = document.querySelector('#last-edited')
 const noteId = location.hash.substring(1)
 let notes = getSavedNotes()
-let note = notes.find((note) => note.id === noteId)
+let note = notes.find(note => note.id === noteId)
 
 if (!note) {
-    location.assign('/index.html')
+  location.assign('/index.html')
 }
 
 titleElement.value = note.title
 bodyElement.value = note.body
 dateElement.textContent = generateLastEdited(note.updatedAt)
 
-titleElement.addEventListener('input', (e) => {
-    note.title = e.target.value
-    note.updatedAt = moment().valueOf()
-    dateElement.textContent = generateLastEdited(note.updatedAt)
-    saveNotes(notes)
+titleElement.addEventListener('input', e => {
+  note.title = e.target.value
+  note.updatedAt = moment().valueOf()
+  dateElement.textContent = generateLastEdited(note.updatedAt)
+  saveNotes(notes)
 })
 
-bodyElement.addEventListener('input', (e) => {
-    note.body = e.target.value
-    note.updatedAt = moment().valueOf()
-    dateElement.textContent = generateLastEdited(note.updatedAt)
-    saveNotes(notes)
+bodyElement.addEventListener('input', e => {
+  note.body = e.target.value
+  note.updatedAt = moment().valueOf()
+  dateElement.textContent = generateLastEdited(note.updatedAt)
+  saveNotes(notes)
 })
 
-removeElement.addEventListener('click', (e) => {
-    removeNote(note.id)
-    saveNotes(notes)
-    location.assign('/index.html')
+removeElement.addEventListener('click', e => {
+  removeNote(note.id)
+  saveNotes(notes)
+  location.assign('/index.html')
 })
 
-window.addEventListener('storage', (e) => {
-    if (e.key === 'notes') {
-        notes = JSON.parse(e.newValue)
-        note = notes.find((note) => note.id === noteId)
+window.addEventListener('storage', e => {
+  if (e.key === 'notes') {
+    notes = JSON.parse(e.newValue)
+    note = notes.find(note => note.id === noteId)
 
-        if (!note) {
-            location.assign('/index.html')
-        }
-
-        titleElement.value = note.title
-        bodyElement.value = note.body
-        dateElement.textContent = generateLastEdited(note.updatedAt)
+    if (!note) {
+      location.assign('/index.html')
     }
+
+    titleElement.value = note.title
+    bodyElement.value = note.body
+    dateElement.textContent = generateLastEdited(note.updatedAt)
+  }
 })
