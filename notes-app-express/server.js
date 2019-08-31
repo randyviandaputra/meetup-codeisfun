2b2faa8c3fd8b1150b83b2673bd0507642956a0a
Randy Vianda Putra <modulloe@gmail.com>
2019-09-01 00:46:39 +0700

    add allow origin

--- notes-app-express/server.js
+++ notes-app-express/server.js
@@ -5,46 +5,50 @@ const bodyParser = require('body-parser')
 const db = require('./db/db')
 
 // set up exprees
-const app = express();
+const app = express()
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  )
+  next()
+})
 
 // body parser
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}))
 
 // get all data
 app.get('/api/v1/todos', (req, res) => {
   res.status(200).send({
     success: true,
     message: 'todos berhasil di ambil',
-    todos: db
+    todos: db,
   })
 })
 
 // post data
 app.post('/api/v1/todos', (req, res) => {
-  // req.body.{key object}
-  if (!req.body.title) {
+  // return res.send(req.body)
+  if (!req.body.description) {
     return res.status(400).send({
       success: false,
-      message: 'title is required'
-    })
-  } else if (!req.body.description) {
-    return res.status(400).send({
-      success: false,
-      message: 'description is required'
+      message: 'description is required',
     })
   }
 
   const todo = {
     id: db.length + 1,
-    title: req.body.title,
-    description: req.body.description
+    description: req.body.description,
+    completed: false,
   }
   db.push(todo)
   return res.status(201).send({
     success: true,
-    message: "todo berhasil di tambah",
-    todo
+    message: 'todo berhasil di tambah',
+    todos: db,
   })
 })
 
@@ -52,18 +56,18 @@ app.post('/api/v1/todos', (req, res) => {
 // url : /api/v1/todos/1
 app.get('/api/v1/todos/:id', (req, res) => {
   const id = parseInt(req.params.id)
-  db.map((todo) => {
+  db.map(todo => {
     if (todo.id === id) {
       return res.status(200).send({
         success: true,
         message: 'todo berhasil di ambil',
-        todo
+        todo,
       })
     }
   })
   return res.status(404).send({
     success: false,
-    message: 'data tidak ditemukan'
+    message: 'data tidak ditemukan',
   })
 })
 
@@ -75,13 +79,13 @@ app.delete('/api/v1/todos/:id', (req, res) => {
       db.splice(index, 1)
       return res.status(200).send({
         success: true,
-        message: 'todo berhasil di hapus'
+        message: 'todo berhasil di hapus',
       })
     }
   })
   return res.status(404).send({
     success: false,
-    message: 'data tidak ditemukan'
+    message: 'data tidak ditemukan',
   })
 })
 
@@ -100,19 +104,19 @@ app.put('/api/v1/todos/:id', (req, res) => {
   if (!findTodo) {
     return res.status(404).send({
       success: false,
-      message: 'todo not found'
+      message: 'todo not found',
     })
   }
 
   if (!req.body.title) {
     return res.status(400).send({
       success: false,
-      message: 'title is required'
+      message: 'title is required',
     })
   } else if (!req.body.description) {
     return res.status(400).send({
       success: false,
-      message: 'description is required'
+      message: 'description is required',
     })
   }
 
@@ -127,7 +131,7 @@ app.put('/api/v1/todos/:id', (req, res) => {
   return res.status(201).send({
     success: true,
     message: 'berhasil di update',
-    updatedTodo
+    updatedTodo,
   })
 })
 
